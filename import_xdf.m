f394ad914671f9f770cf0583f242a77aa687ad7f
Sydney Kessler <sydney@jumping.ucsd.edu>
2021-02-25 13:04:15 -0800

    added no bsl opt

--- import_xdf.m
+++ import_xdf.m
@@ -28,12 +28,14 @@ for i=1:length(files)
        try
            [EEG_2] = pop_loadxdf(baseFileName,'streamtype','EEG');
        catch
-           warning('Importing failture, skipping: %s', baseFileName);
+           warning('Importing failure, skipping: %s', baseFileName);
        end
        % merge with OG file
        if exist('EEG_2', 'var')  % check if new EEG struct exists
            EEG = pop_mergeset(EEG,EEG_2, 0);  % merge
-           clearvars EEG_2  % clearb for next trial
+           clearvars EEG_2  % clear for next trial
+           % there's a strjoin problem, try running this:
+           % which strjoin -all
        end
    end    
 end
