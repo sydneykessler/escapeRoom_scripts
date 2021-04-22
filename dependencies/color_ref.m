1596e0d1a2bcef9b7c4c7d4b5f5b1a8ef38d526f
sydneykessler <sydney@jumping.ucsd.edu>
2021-04-22 10:59:55 -0700

    added colors for ps

--- dependencies/color_ref.m
+++ dependencies/color_ref.m
@@ -32,8 +32,8 @@ mint = [0;250;154];
 % DeepSkyBlue
 mediumblue = [0;191;255];
 
-% MediumBLue
-darkblue = [0;191;255];
+% DodgerBlue4 
+darkblue = [16;78;139];
 
 % NavyBlue
 navy = [0;0;128];
@@ -46,11 +46,21 @@ purple = [125;38;205];
 
 %% creating cell array
 
-legend_labels = {'Happy = yellow', 'Angry = red', 'Sad = dark blue'...
+emotion_legend_labels = {'Happy = yellow', 'Angry = red', 'Sad = dark blue'...
     'Thinking = blue', 'Questioning = pink', 'Lost = dark green', ...
     'Ambivalent = mint'};   
 
 color_ref_ = cell(2, 7);
 
+%%
+puzzle_legend_labels = {'1 = blue', '2 = darkblue', '3 = navy'...
+    'Finished = violet'};  
 
+% puzzle_ref = cell(2,4);
+% puzzle_labels = {'1', '2', '3', 'Finished'};
+% puzzle_ref(1,:) =  puzzle_labels;
+% puzzle_ref(2,1) = {mediumblue ./ 256};
+%...
+% color_ref(1).puzzle_ref =  puzzle_ref;
+% save('color_reference.mat','color_ref')
 
